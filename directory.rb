361a5535fd7a780e45ac8702a17984758e0db707
Denis <denisglb@yahoo.co.uk>
2017-07-19 15:18:57 +0100

    Added Index value

--- directory.rb
+++ directory.rb
@@ -1,18 +1,24 @@
 #let's put all the students in an array
 
-students = [
-	{name: "Dr. Hannibal Lecter",cohort: :november},
-	{name: "Darth Vader", cohort: :november},
-	{name: "Nurse Ratched", cohort: :november},
-	{name: "Michael Corleone", cohort: :november},
-	{name: "Alex DeLarge", cohort: :november},
-	{name: "The Wicked Witch of the West", cohort: :november},
-	{name: "Terminator", cohort: :november},
-	{name: "Freddy Krueger", cohort: :november},
-	{name: "The Joker", cohort: :november},
-	{name: "Joffrey Baratheon", cohort: :november},
-	{name: "Norman Bates", cohort: :november}
-]
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+	students = []
+	# get the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do 
+		# add the student has to the array
+		students << {name: name, cohort: :november}
+		puts "now we have #{students.count} students"
+		# get another name for the user
+		name =gets.chomp
+	end
+	students 
+end
+
+students = input_students
 
 def print_header
 	puts "The students of Villains Academy"
@@ -20,8 +26,8 @@ def print_header
 end
 def print(students)
 # iteration over the students, this will print all the students' names
-students.each do |student|
-	puts "#{student[:name]} (#{student[:cohort]} cohort)"
+students.each_with_index do |student, index|
+	puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 
@@ -29,3 +35,5 @@ def print_footer(names)
 # finally, we print the total number of students
 puts "Overall, we have #{names.count} great students"
 end
+
+print students
