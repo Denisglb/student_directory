e90a8e8b6ebc1b5d8aae83d123bdd6f2f5c30796
Denis <denisglb@yahoo.co.uk>
2017-07-26 14:05:59 +0100

    8.12 Loading data

--- directory.rb
+++ directory.rb
@@ -9,22 +9,22 @@ def input_students
 	students = []
 	# get the first name
 	# name = gets.chomp
-	name = gets.gsub(/\n/, "")
+	name = STDIN.gets.chomp
 	
 	puts "Enter the cohort".center(50)
-	cohort = gets.gsub(/\n/,"")
+	cohort = STDIN.gets.chomp
 
 	puts "Please add a country of birth for the student".center(50)
 	# birth_place = gets.chomp
-	birth_place = gets.gsub(/\n/,"")
+	birth_place = STDIN.gets.chomp
 	
 	puts "Please add the height of the student in cm".center(50)
 	# height = gets.chomp
-	height = gets.gsub(/\n/, "")
+	height = STDIN.gets.chomp
 	
 	puts "Please add a hobbie for the student".center(50)
 	# hobbie = gets.chomp
-	hobbie = gets.gsub(/\n/, "")
+	hobbie = STDIN.gets.chomp
 	
 	# while the name is not empty, repeat this code
 	while !name.empty? do 
@@ -40,18 +40,18 @@ def input_students
 		end
 		# get another name for the user
 		puts "Please enter the next students' name or press enter to quite".center(50)
-		name = gets.chomp
+		name = STDIN.gets.chomp
 		break if name == ""
 		puts "Enter the cohort".center(50)
-		cohort = gets.gsub(/\n/,"")
+		cohort = STDIN.gets.chomp
 		puts "Please add a country of birth for the student".center(50)
-		birth_place = gets.chomp
+		birth_place = STDIN.gets.chomp
 
 		puts "Please add the height of the student in cm".center(50)
-		height = gets.chomp
+		height = STDIN.gets.chomp
 
 		puts "Please add a hobbie for the student".center(50)
-		hobbie = gets.chomp
+		hobbie = STDIN.gets.chomp
 	end
 	@students 
 end
@@ -91,7 +91,7 @@ def interactive_menu
 	# declared the variable students before the loop setting it to an empty array. Done so that it will be availble in several iterations of the loop. 
 	loop do 
 		print_menu
-		process(gets.chomp)
+		process(STDIN.gets.chomp)
 	end
 end
 
@@ -99,6 +99,7 @@ def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
 	puts "3. Save the list of students.csv"
+	puts "4. Load the list from students.csv"
 	puts "9. Exit"
 end
 
@@ -116,6 +117,8 @@ def process (selection)
 			show_students
 		when "3"
 			save_students
+		when "4"
+			load_students
 		when "9"
 			exit
 		else 
@@ -130,12 +133,32 @@ def save_students
 	# iterate over the array of students
 	@students.each do |student|
 		student_data = [student[:name], student[:cohort], student[:country], student [:height], student [:hobbie]]
-		csv_line = student_data.join(",")
+		csv_line = student_data.join(',')
 		file.puts csv_line
 	end
 	file.close
 	# every time a file is opened it needs to be closed
 end
 
+def load_students(filename = "students.csv")
+	file = File.open(filename,"r")
+	file.readlines.each do |line|
+		name, cohort, country, height, hobbie = line.chomp.split(',')
+		@students << {name: name, cohort: cohort.to_sym, birth_place: country.to_sym, height: height.to_sym, hobbie: hobbie.to_sym}
+	end
+	file.close
+end
+
+def try_load_students
+	filename = 	ARGV.first #first argument from the command line
+	return if filename.nil? #get out od the method if it isn'ts given
+	if File.exist?(filename) #if it exists
+		load_students(filename)
+		puts "loaded #{@students.count} from #{filename}"
+	else
+		puts "Sorry, #{filename} doesn't exist."
+		exit #quit the program
+	end
+end
 
 interactive_menu
